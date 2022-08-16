baeffe5949e0c8708684ec1a95d8adde08bc995e
Kyungwook Nam <kyungwook.nam@cern.ch>
2022-08-16 21:22:57 +0200

    work in progress

--- test/skim.py
+++ test/skim.py
@@ -6,18 +6,30 @@ parser=argparse.ArgumentParser()
 parser.add_argument("--in_root",type=str,default="test.root",help="input root file name")
 parser.add_argument("--save_name",type=str,default="test_skimmed.root",help="skimmed root file name")
 
+parser.add_argument("--mid",type=str,default="",help="mid list 1,2,3  empty for all mid")
+parser.add_argument("--channel",type=str,default="",help="channel list 1,2,3  empty for all channel")
+
+parser.add_argument("--thr_ps",type=float,default=9999.,help="veto threshold for preshower")
+parser.add_argument("--thr_md",type=float,default=9999.,help="veto threshold for muon detector")
+
 parser.add_argument("--mod",type=str,default="fast",help="fast or wave")
 parser.add_argument("--mapping",type=str,default="mapping_data_MCPPMT_positiveSignal.csv",help="mapping file")
 parser.add_argument("--pedestal",type=str,default="ped_236.csv",help="pedestal file")
 
 args=parser.parse_args()
 mod=args.mod
+if(args.mid==""):list_mid=[]
+else:list_mid=[int(i) for i in args.mid.split(",")]
+if(args.channel==""):list_channel=[]
+else:list_channel=[int(i) for i in args.channel.split(",")]
+thr_ps = args.thr_ps
+thr_md = args.thr_md
 
 # open root file and get Tree
-in_file = ROOT.TFile(args.in_root, "READ")
-out_file = ROOT.TFile(args.save_name, "RECREATE")
+infile = ROOT.TFile(args.in_root, "READ")
+outfile = ROOT.TFile(args.save_name, "RECREATE")
 
-atree = in_file.Get("events")
+atree = infile.Get("events")
 atree.GetEntry(0)
 anevt = getattr(atree,"TBevt")
 # anevt = ROOT.TBevt("TBfastmode")()
@@ -41,6 +53,8 @@ for ievt in range(atree.GetEntries()):
     adc_ps = 0.
     adc_md = 0.
 
+    if(list_mid==[]):list_mid=range(anevt.size())
+    if(list_channel==[]):list_channel=range(channelsize)
     # check data of each entry
     for imid in list_mid:
         for ich in list_channel:
@@ -62,3 +76,5 @@ for ievt in range(atree.GetEntries()):
 
     btree.Fill()
 print("Entry ended")
+
+outfile.WriteObject(btree, "events")
